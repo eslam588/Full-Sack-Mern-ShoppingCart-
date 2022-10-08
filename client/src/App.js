07c5284acb2eb3e25c61fb1a8a8460642dec2788
eslam588 <egmal63@gmal.com>
2022-10-08 18:11:50 +0200

    filter-comp

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import data from './data.json';
 import Products from './components/Products/Products';
+import Filter from './components/Filter/Filter';
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
         <main>
           <div className="wrapper">
             <Products products={products} />
-            <div className="filter-wrapper">filter</div>
+            <Filter/>
           </div>
         </main>
         <Footer/>
