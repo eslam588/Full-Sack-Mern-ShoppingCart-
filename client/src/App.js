75e568f1884ccbf3e3247f93a0807a8715b9e5bf
eslam588 <egmal63@gmal.com>
2022-10-08 19:25:14 +0200

    filterpart2

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,42 @@ import Filter from './components/Filter/Filter';
 
 function App() {
   const [products,setProducts]=useState(data);
+  const [size, setSize] = useState("");
+  const [sort, setSort] = useState("");
+ 
+
+  const handleFilterBySize = (e) => {
+    setSize(e.target.value);
+    if(e.target.value == "ALL") {
+      setProducts(data);
+
+    }
+    else {
+      let cloneProducts = [...data];
+      let filteredProducts=cloneProducts.filter(product => product.sizes.indexOf(e.target.value) != -1);
+      console.log(filteredProducts);
+      setProducts(filteredProducts);
+    }
+    
+  }
+
+  const handleFilterBySort = (e) => {
+    setSort(e.target.value);
+    let order= e.target.value;
+    let cloneProducts = [...data];
+    let newProducts = cloneProducts.sort( function(a,b){
+      if(order == "lowest"){
+        return a.price - b.price
+      }
+      else if(order == "highest"){
+        return b.price - a.price
+      }
+      if(order == "latest"){
+        return a.id < b.id ? 1 : -1
+      }
+    });
+    setProducts(newProducts);
+  }
 
   return (
       <div className="layout">
@@ -15,7 +51,7 @@ function App() {
         <main>
           <div className="wrapper">
             <Products products={products} />
-            <Filter/>
+            <Filter handleFilterBySize={handleFilterBySize} handleFilterBySort={handleFilterBySort} size={size} sort={sort}/>
           </div>
         </main>
         <Footer/>
