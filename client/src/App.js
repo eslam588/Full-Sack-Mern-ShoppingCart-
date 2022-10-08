7fb40a40dcf432ee3d2820dcbbc0f22d001fdd1f
eslam588 <egmal63@gmal.com>
2022-10-08 16:15:44 +0200

    products comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import React , {useState} from 'react'
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import data from './data.json';
+import Products from './components/Products/Products';
+
 
 function App() {
+  const [products,setProducts]=useState(data);
+
   return (
       <div className="layout">
        <Header/>
         <main>
-          content
+          <div className="wrapper">
+            <Products products={products} />
+            <div className="filter-wrapper">filter</div>
+          </div>
         </main>
         <Footer/>
       </div>
